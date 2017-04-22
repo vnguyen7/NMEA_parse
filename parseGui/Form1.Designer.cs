0d273d737c065e78f0c645385a07a2c8e61a8214
vinh nguyen <vnguyen1415@gmail.com>
2017-04-22 00:30:40 -0400

    Porject Upload

---
+++ parseGui/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace parseGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbPort = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtReceived = new System.Windows.Forms.RichTextBox();
+            this.availPort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbPort
+            // 
+            this.lbPort.AutoSize = true;
+            this.lbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPort.Location = new System.Drawing.Point(50, 28);
+            this.lbPort.Name = "lbPort";
+            this.lbPort.Size = new System.Drawing.Size(40, 16);
+            this.lbPort.TabIndex = 0;
+            this.lbPort.Text = "Port:";
+           
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(266, 25);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(93, 26);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(375, 25);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(86, 26);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(386, 219);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 5;
+            this.btnReceive.Text = "Clear";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // txtReceived
+            // 
+            this.txtReceived.Location = new System.Drawing.Point(88, 57);
+            this.txtReceived.Name = "txtReceived";
+            this.txtReceived.Size = new System.Drawing.Size(373, 144);
+            this.txtReceived.TabIndex = 6;
+            this.txtReceived.Text = "";
+            // 
+            // availPort
+            // 
+            this.availPort.AutoSize = true;
+            this.availPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.availPort.Location = new System.Drawing.Point(96, 28);
+            this.availPort.Name = "availPort";
+            this.availPort.Size = new System.Drawing.Size(29, 15);
+            this.availPort.TabIndex = 8;
+            this.availPort.Text = "N/A ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 277);
+            this.Controls.Add(this.availPort);
+            this.Controls.Add(this.txtReceived);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lbPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReceive;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox txtReceived;
+        private System.Windows.Forms.Label availPort;
+    }
+}
+
---
+++ parseGui/Form1.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management; 
+
+namespace parseGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //disabled close btn 
+            btnClose.Enabled = false;
+
+            //get all available port
+            string[] ports = SerialPort.GetPortNames();
+            //Find the port that contain u-blox 7 vendor id 1546 and product id 01A7
+            List<string> vid = ComPortNames("1546","01A7");
+            foreach (string s in ports)
+            {
+                if (vid.Contains(s))
+                {
+                    //set text for availPort label 
+                    availPort.Text = s;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        /* Private function to get vendor id and product id of ublox gps device. 
+         * Get the ublox gps id from device manager. 
+         * Create pattern by passing the vid and pid into the parameter. 
+         * Generate a regex formula and ignore case sensitive. 
+         * OpenSubKey : open the subfolder inside the HKEY_LOCAL_MACHINE registra 
+         * GetSubKeyNames: list all the subfolder's name 
+         * HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\USB\VID_1546&PID_01A7\5&376aba2d&0&5\Device Parameters
+         */
+        private List<string> ComPortNames(String VID, String PID) 
+        {
+            String pattern = String.Format("^VID_{0}.PID_{1}", VID, PID);
+            Console.WriteLine(pattern + "pattern"); 
+            Regex _rx = new Regex(pattern, RegexOptions.IgnoreCase);
+            List<string> comports = new List<string>();
+            RegistryKey rk1 = Registry.LocalMachine;
+            RegistryKey rk2 = rk1.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum");
+
+            //Go into enum subfolder in HKEY_LOCAL_MACHINE
+            foreach (String s3 in rk2.GetSubKeyNames())
+            {
+                //consider only when the subfolder is USB 
+                 if (s3 == "USB")
+                { 
+                    RegistryKey rk3 = rk2.OpenSubKey(s3);
+                    foreach (String s in rk3.GetSubKeyNames())
+                    {
+                        //if match with the regex VID_0156&PID_01A7
+                        if (_rx.Match(s).Success)
+                        {
+                            RegistryKey rk4 = rk3.OpenSubKey(s);
+
+                            foreach (String s2 in rk4.GetSubKeyNames())
+                            {
+                                Console.WriteLine(s2);
+                                RegistryKey rk5 = rk4.OpenSubKey(s2);
+                                Console.WriteLine("rk5:" + rk5);
+                                RegistryKey rk6 = rk5.OpenSubKey("Device Parameters");
+                                Console.WriteLine("rk6:" + rk6);
+                                //Get port name and return 
+                                comports.Add((string)rk6.GetValue("PortName"));
+                            }
+                        }
+                 }
+                }
+            }
+            return comports;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+            try
+            {
+              //set port to serialPort1 to open 
+                serialPort1.PortName = availPort.Text; 
+                serialPort1.Open();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "messsage", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "messsage", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtReceived.Clear();
+        }
+
+        private string rxString, parseString="";
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            rxString = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(displayText));
+        }
+
+        /*
+       $GPGLL,4916.45,N,12311.12,W,225444,A,*1D
+            GLL: geographic position, latitude and longitude (dd mm,mmmm format 0-7 decimal places) 
+            4916.45,N: Latitude 49 deg. 16.45 min. North    ( N / S) 
+            12311.12,W: Longitude 123 deg. 11.12 min. West  (W / E) 
+            225444 : Fix taken at 22:5444 UTC            (hhmmss.ss) 
+            A: data Active or V (void)
+            *1D: checksum data 
+        */
+        private string ParseGLL(string sentence)
+        {
+            //check for NMEA maximum length 
+            if (sentence.Length < 82)
+            {
+                string[] arr = sentence.Split(',');
+                //case of GLL,and check if enough information of latitude, longtitude to parse 
+                if ((arr[0] == "$GPGLL") && (arr[1] != "") && (arr[2] == "N" || arr[2] == "S")
+                    && (arr[3] != "") && (arr[4] == "W" || arr[4] == "E"))
+                {
+                    // Append latitude: first 2 digit deg, last 4 digit is min, north/south 
+                    string Latitude = arr[1].Substring(0, 2) + " deg";
+                    Latitude = Latitude + arr[1].Substring(2) + " min";
+                    if (arr[3] == "N")
+                        Latitude = Latitude + " North ";
+                    else Latitude = Latitude + " South ";
+
+                    //Append longitude: first 
+                    string Longitude = arr[3].Substring(0, 2) + " deg" + arr[3].Substring(3) + " min";
+                    if (arr[3] == "E")
+                        Longitude = Longitude + " East ";
+                    else Longitude = Longitude + " West ";
+
+                    //Append UTC time hhmmss.ss
+                    string time = arr[5].Substring(0, 2) + ":" + arr[5].Substring(2, 2) + ":" + arr[5].Substring(4) + " UTC";
+                    string pos;
+                    //valid data or not?
+                    if (arr[6] == "A")
+                        pos = "valid data\n";
+                    else pos = "unvalid data\n";
+
+                    parseString = "Lattitude: "+Latitude + "Longitude: " + Longitude + " " + time + " " + pos +"\n";
+                }
+                else if (arr[0] == "$GPGLL")
+                {
+                    parseString = "invalid data " + arr[5].Substring(0, 2) + ":" + arr[5].Substring(2, 2) + ":" + arr[5].Substring(4) + " UTC\n";
+                }
+            }
+            else parseString = "invalid NMEA Length"; 
+            return parseString;
+        }
+       // string text; 
+        private void displayText(object o,EventArgs e)
+        {
+           // text = "$GPGLL,4916.45,N,12311.12,W,225444,A,*1D";
+            parseString = ParseGLL(rxString); 
+            Console.WriteLine(parseString); 
+            txtReceived.AppendText(parseString); 
+        }
+      
+    }
+}
